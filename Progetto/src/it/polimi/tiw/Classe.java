9a07544a3d4356908b6ff489ee6355d74da945b4
giacomocassano <48253515+giacomocassano@users.noreply.github.com>
2021-03-05 16:12:15 +0100

    Conflict test

--- Progetto/src/it/polimi/tiw/Classe.java
+++ Progetto/src/it/polimi/tiw/Classe.java
@@ -3,6 +3,6 @@ package it.polimi.tiw;
 public class Classe {
 	public static void main(String[] argv) {
 		System.out.println("TENNIS >> PALLACESTO");
-		int variabile = 6;
+		int variabile = 7;
 	}
 }
