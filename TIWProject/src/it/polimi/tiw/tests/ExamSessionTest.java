7009be4963596cb16b402e401eef4248b26cf8a1
giacomocassano <48253515+giacomocassano@users.noreply.github.com>
2021-03-31 15:47:22 +0200

    Added getReportedGrades() in ExamSessionDAO and relative test

--- TIWProject/src/it/polimi/tiw/tests/ExamSessionTest.java
+++ TIWProject/src/it/polimi/tiw/tests/ExamSessionTest.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
-import org.apache.catalina.filters.CsrfPreventionFilter;
-
 import it.polimi.tiw.beans.ExamResult;
 import it.polimi.tiw.beans.Student;
 import it.polimi.tiw.dao.ExamSessionDAO;
@@ -18,7 +16,8 @@ public class ExamSessionTest {
 		//testGetRegisteredStudentsResults();
 		//testGetStudentExamResult();
 		//testRejectExamResult();
-		testUpdateExamResult();
+		//testUpdateExamResult();
+		testGetReportedGrades();
 	}
 	
 	public static void testGetRegisteredStudentsResults() {
@@ -139,6 +138,36 @@ public class ExamSessionTest {
 			//GradeStatus should remain PUBBLICATO
 			System.out.println("Exam grade state is: " +result.getGrade() + laude +" - "+result.getGradeStatus());
 			
+			
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void testGetReportedGrades() {
+		System.out.println("Test for ExamSessionDAO getReportedGrades()");
+		try {
+			String driver = "com.mysql.cj.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/polionline";
+			String user = "poliadmin";
+			String dbpassword = "Gruppo27";
+			
+			Class.forName(driver);
+			Connection connection = DriverManager.getConnection(url, user, dbpassword);
+
+			ExamSessionDAO examSessionDAO = new ExamSessionDAO(connection);
+			Timestamp timestamp = Timestamp.valueOf("2020-01-14 12:00:00");
+			List<ExamResult> results = examSessionDAO.getReportedGrades(1, timestamp);
+			
+			System.out.println("Size: " + results.size());
+			for(ExamResult result: results) {
+				System.out.println(result.getExamSession().getCourse().getName() + " - " + result.getExamSession().getDateTime() + " - " + result.getStudent().getLastName() + " " + result.getStudent().getFirstName() + " - " + result.getGrade() + " - " + result.getGradeStatus());
+			}
 			
 			if (connection != null) {
 				connection.close();
