f4e4ae52fc6ca33d78beb870852800cc4343f6cd
giacomocassano <48253515+giacomocassano@users.noreply.github.com>
2021-04-06 10:42:01 +0200

    Added some controllers

---
+++ TIWProject/src/it/polimi/tiw/beans/formbeans/LoginForm.java
@@ -0,0 +1,81 @@
+package it.polimi.tiw.beans.formbeans;
+
+public class LoginForm {
+	private String personCode;
+	private String password;
+	private String personCodeError;
+	private String passwordError;
+	private String credentialError;
+	private boolean loginOk;
+	
+	public LoginForm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPersonCode() {
+		return personCode;
+	}
+
+	public void setPersonCode(String personCode) {
+		this.personCode = personCode;
+		if(personCode == null || personCode.isEmpty()) {
+			this.personCodeError = "Person code is required";
+		}else {
+			try {
+				Integer.parseInt(personCode);
+				this.personCodeError = null;
+			} catch (NumberFormatException e) {
+				this.personCodeError = "Person code must contain only numbers.";
+			}
+		}
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+		if(password == null || password.isEmpty()) {
+			this.passwordError = "Password field is required.";
+		}else if(password.length() < 3 || password.length() > 12) {
+			this.passwordError = "Password must be between 3 and 12 characters.";
+		}else {
+			this.passwordError = null;
+		}
+	}
+
+	public String getPersonCodeError() {
+		return personCodeError;
+	}
+
+
+	public String getPasswordError() {
+		return passwordError;
+	}
+
+	public void setLoginOk(boolean valid) {
+		if(valid) {
+			this.loginOk = true;
+			this.credentialError = null;
+		}else {
+			this.loginOk = false;
+			this.credentialError = "Username and password are not correct.";
+		}
+	}
+	
+	public boolean areCredetialsOk() {
+		return this.passwordError == null && this.personCodeError == null;
+	}
+	
+	public boolean isLoginOk() {
+		return loginOk;
+	}
+
+	public String getCredentialError() {
+		return credentialError;
+	}
+	
+	
+}
---
+++ TIWProject/src/it/polimi/tiw/controllers/ReportExamSessionGrades.java
@@ -0,0 +1,84 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+/**
+ * Servlet implementation class ReportExamSessionGrades
+ */
+@WebServlet("/ReportExamSessionGrades")
+public class ReportExamSessionGrades extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+    private TemplateEngine templateEngine;
+    
+    @Override
+    public void init() throws ServletException {
+    	ServletContext context = getServletContext();
+    	ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+    	try {
+			String driver = context.getInitParameter("dbDriver");
+			String url = context.getInitParameter("dbUrl");
+			String user = context.getInitParameter("dbUser");
+			String password = context.getInitParameter("dbPassword");
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new UnavailableException("Can't load database driver");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new UnavailableException("Couldn't get db connection");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int courseId = Integer.parseInt(request.getParameter("courseId"));
+		Timestamp datetime = Timestamp.valueOf(request.getParameter("date"));
+		
+		System.out.println("Course: " + courseId);
+		System.out.println("Date: " + datetime);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	@Override
+	public void destroy() {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+	}
+
+}
